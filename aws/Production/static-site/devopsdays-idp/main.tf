a2a6f38e49c65f6ac7f7f723db54aa28665ec9a8
Fillipe Paz <fillipeapaz@gmail.com>
2024-04-18 01:13:29 -0300

    Criando Site Estático para o projeto devopsdays-idp no ambiente Production

---
+++ aws/Production/static-site/devopsdays-idp/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.45.0"
+    }
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = "versioning-terraform-home"
+    key    = "site-estatico/Production/devopsdays-idp/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+
+module "cloudfront" {
+    source = "git@github.com:fillipepaz/aws-cloudfront-module.git//static-site"
+    accountId="704151674151"
+    repoNameAndOrg="/static-site-example"
+    bucketName="bucket-devopsdays-idp"
+    project="devopsdays-idp" 
+    
+}
